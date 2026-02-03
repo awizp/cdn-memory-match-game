a03920563f0267e2ec0b4e92ebcbef6f687ea204
Awizp <ridvishnu@gmail.com>
2026-02-03 06:52:28 +0530

    fix: major ui and functionalities added

--- src/main.js
+++ src/main.js
@@ -1,150 +1,206 @@
-// variables,
-const movesEl = document.querySelector(".game-moves > p");
-const timeEl = document.querySelector(".game-time > p");
-const matchEl = document.querySelector(".game-match > p");
+document.addEventListener('DOMContentLoaded', () => {
 
-const gameBoardEl = document.querySelector(".game-board");
-const gameModalEl = document.querySelector(".game-modal");
+    // variables,
+    const movesEl = document.getElementById("game-moves");
+    const timeEl = document.getElementById("game-time");
+    const matchEl = document.getElementById("game-match");
 
-const resultMoves = document.querySelector(".result-moves");
-const resultTime = document.querySelector(".result-time");
+    const gameBoardEl = document.querySelector(".game-board");
+    const gameModalEl = document.querySelector(".game-modal");
 
-const startBtn = document.getElementById("game-start");
-const playAgainBtn = document.getElementById("game-playAgain");
-let fragmentEl = document.createDocumentFragment();
+    const resultMoves = document.querySelector(".result-moves");
+    const resultTime = document.querySelector(".result-time");
 
-const images = [
-    "/image/dog1.jpg",
-    "/image/dog2.jpg",
-    "/image/dog3.jpg",
-    "/image/dog4.jpg",
-    "/image/dog5.jpg",
-    "/image/dog6.jpg",
-    "/image/dog7.jpg",
-    "/image/dog8.jpg",
-];
+    const startBtn = document.getElementById("game-start");
+    const playAgainBtn = document.getElementById("game-playAgain");
 
-let firstCard = null;
-let secondCard = null;
-let flipStatus = true;
-let timeRunning = true;
-let timerRunningId;
+    let fragmentEl = document.createDocumentFragment();
 
-let moves = 0;
-let matches = 0;
-let seconds = 0;
+    const images = [
+        "dog1.jpg",
+        "dog2.jpg",
+        "dog3.jpg",
+        "dog4.jpg",
+        "dog5.jpg",
+        "dog6.jpg",
+        "dog7.jpg",
+        "dog8.jpg",
+    ];
 
-const suffleCards = (card) => {
-    card.sort(() => Math.random() - 0.5);
-};
+    // game states for elements,
+    let firstCard = null;
+    let secondCard = null;
+    let flipStatus = true;
+    let timeRunning = false;
+    let timerInterval;
 
-const gameBoard = () => {
-    let cardImages = images.concat(images);
+    // game stat values,
+    let moves = 0;
+    let matches = 0;
+    let seconds = 0;
 
-    suffleCards(cardImages);
+    // suffle the cards before append,
+    const shuffleCards = (card) => {
+        for (let i = card.length - 1; i > 0; i--) {
+            let randomNum = Math.floor(Math.random() * (i + 1));
 
-    for (let i = 0; i < cardImages.length; i++) {
-        let cardEl = document.createElement('div');
-        cardEl.className = 'game-card';
-        cardEl.innerHTML = `
-            <div class="card-front"><ion-icon name="heart"></ion-icon></div>
-          <div class="card-back"><img src="${cardImages[i]}" alt="dog-image1"></div>
-        `;
-        cardEl.dataset.image = cardImages[i];
-        cardEl.addEventListener('click', (e) => flippingCard(e));
-        fragmentEl.append(cardEl);
-    }
+            [card[i], card[randomNum]] = [card[randomNum], card[i]];
+        }
+    };
 
-    gameBoardEl.append(fragmentEl);
+    // update the stats on every time,
+    const updateStats = () => {
+        movesEl.textContent = moves;
+        matchEl.textContent = matches + "/8";
 
-    firstCard = null;
-    secondCard = null;
-    flipStatus = true;
-    timeRunning = true;
+        let minEl = Math.floor(seconds / 60);
+        let secEl = seconds % 60;
 
-    moves = 0;
-    matches = 0;
-    seconds = 0;
+        if (minEl < 10) minEl = "0" + minEl;
+        if (secEl < 10) secEl = "0" + secEl;
 
-    updateStats();
-    clearInterval(timerRunningId);
-};
+        timeEl.textContent = `${minEl} : ${secEl}`;
+    };
 
-gameBoard();
+    // starting the timer,
+    const startTimer = () => {
+        timeRunning = true;
+        timerInterval = setInterval(() => {
+            seconds++;
+            updateStats();
+        }, 1000);
+    };
 
-const flippingCard = (e) => {
+    // resetting the cards value,
+    const resetCards = () => {
+        firstCard = null;
+        secondCard = null;
+        flipStatus = true;
+    };
 
-    let cardParent = e.target;
+    // debounce method,
+    const debounce = (func, delay = 500) => {
+        let timeoutFunc;
 
-    if (!flipStatus) return;
+        return () => {
+            clearTimeout(timeoutFunc);
+            timeoutFunc = setTimeout(() => {
+                func();
+            }, delay);
+        };
+    };
 
-    if (cardParent.classList.contains("matched")) return;
-    if (cardParent.classList.contains("flipped")) return;
+    // ending the game
+    const endGame = () => {
+        clearInterval(timerInterval);
 
-    if (firstCard == null) {
-        firstCard = cardParent;
-    } else {
-        secondCard = cardParent;
-        flipStatus == false;
-        moves++;
+        resultMoves.textContent = movesEl.textContent;
+        resultTime.textContent = timeEl.textContent;
+        gameModalEl.style.display = 'flex';
+        moves = seconds = matches = 0;
+        timeRunning = false;
+    };
+
+    // check the two cards if matches,
+    const checkMatch = () => {
+        let match = firstCard.dataset.image == secondCard.dataset.image;
+
+        if (match) {
+            setTimeout(() => {
+                firstCard.classList.add("matched");
+                secondCard.classList.add("matched");
+                matches++;
+
+                updateStats();
+                resetCards();
+
+                if (matches == 8) {
+                    endGame();
+                }
+
+            }, 500);
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove("flipped");
+                secondCard.classList.remove("flipped");
+                resetCards();
+            }, 1250);
+        }
 
         updateStats();
-    }
-};
+    };
 
-const checkMatch = () => {
-    let match = firstCard.dataset.image == secondCard.dataset.image;
+    // card flipping after game board,
+    const flippingCard = (e) => {
 
-    if (match) {
-        setTimeout(() => {
-            firstCard.classList.add("matched");
-            secondCard.classList.add("matched");
-            matches++;
+        let cardParent = e.target;
 
-            if (matches == 8) {
-                endGame();
-                resetStats();
-            }
+        if (!flipStatus) return;
 
-        }, 500);
-    } else {
-        setTimeout(() => {
-            firstCard.classList.remove("flipped");
-            secondCard.classList.remove("flipped");
-        }, 1000);
-    }
+        if (cardParent.classList.contains("matched")) return;
+        if (cardParent.classList.contains("flipped")) return;
 
-    updateStats();
-};
+        cardParent.classList.add('flipped');
 
-const updateStats = () => {
-    movesEl.textContent = moves;
-    matchEl.textContent = matches + "/8";
+        if (firstCard == null) {
+            firstCard = cardParent;
+        } else {
+            secondCard = cardParent;
+            flipStatus == false;
+            moves++;
 
-    let minEl = Math.floor(seconds / 60);
-    let secEl = seconds % 60;
+            updateStats();
+            checkMatch();
+        }
+    };
 
-    if (min < 10) return "0" + minEl;
-    if (secEl < 10) return "0" + secEl;
+    // creating game board,
+    const gameBoard = () => {
+        let cardImages = images.concat(images);
+        gameBoardEl.innerHTML = "";
 
-    timeEl.textContent = `${minEl} : ${secEl}`;
-};
+        shuffleCards(cardImages);
 
-const resetStats = () => {
-    firstCard = null;
-    secondCard = null;
-    flipStatus = true;
-};
+        for (let i = 0; i < cardImages.length; i++) {
+            let cardEl = document.createElement('div');
+            cardEl.className = 'game-card';
+            cardEl.innerHTML = `
+                <div class="card-front pointer-events-none">
+                    <i class="fa-solid fa-spider pointer-events-none"></i>
+                </div>
+                <div class="card-back pointer-events-none">
+                    <img src="/image/${cardImages[i]}" alt="dog-image${i}"/>
+                </div>
+        `;
+            cardEl.dataset.image = cardImages[i];
+            cardEl.addEventListener('click', (e) => debounce(flippingCard(e)));
+            fragmentEl.append(cardEl);
+        }
 
-const endGame = () => {
-    timerRunningId = clearInterval(() => {
-        gameModalEl.style.display = "flex";
-    });
+        gameBoardEl.append(fragmentEl);
 
-    timeRunning = false;
+        firstCard = null;
+        secondCard = null;
+        flipStatus = true;
+        timeRunning = true;
 
-    resultMoves.textContent = movesEl.textContent;
-    resultTime.textContent = timeEl.textContent;
+        moves = 0;
+        matches = 0;
+        seconds = 0;
 
-    resetStats();
-};
+        updateStats();
+    };
+
+    const startGame = () => {
+        if (!timeRunning) {
+            startTimer();
+        }
+
+        gameModalEl.style.display = 'none';
+        gameBoard();
+        startBtn.style.display = 'none';
+    };
+
+    startBtn.addEventListener('click', debounce(startGame));
+    playAgainBtn.addEventListener('click', debounce(startGame));
+});
